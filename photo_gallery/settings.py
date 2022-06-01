58c7e6a44be414306e8cce812b761133ce3ea8f3
munga114 <mungai.mbugua@student.moringaschool.com>
2022-06-01 11:24:05 +0300

    fix bug

--- photo_gallery/settings.py
+++ photo_gallery/settings.py
@@ -1,11 +1,21 @@
 import dj_database_url
 import django_heroku
 from pathlib import Path
-from decouple import config,Csv
+from decouple import config
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.1/howto/static-files/
+STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
@@ -76,7 +86,6 @@ DATABASES = {
         'USER': config('DB_USER'),
         'PASSWORD': config('DB_PASSWORD'),
         'HOST': config('DB_HOST'),
-        'PORT': '',
     }
 
 }
